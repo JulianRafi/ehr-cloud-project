681bb7c76b7173d1a2797f4c0dd94a759217eead
Cindy Ariestanti <priliariestanti13@gmail.com>
2024-06-06 08:58:53 +0700

    before interface change

--- amplify_shop/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify_shop/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyshop": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyshopee42f9dd": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3cdddc1b0": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
